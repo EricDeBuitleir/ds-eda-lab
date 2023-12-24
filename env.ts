81c884489fa1df7182852532388f5f7a4d78ee7d
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2023-12-24 15:59:22 +0000

    testing on different machine

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
 export const SES_EMAIL_FROM = '20094078@mail.wit.ie' ;
-export const SES_EMAIL_TO =  'ericbutler125@outlook.com';
+export const SES_EMAIL_TO =  '20094078@mail.wit.ie';
